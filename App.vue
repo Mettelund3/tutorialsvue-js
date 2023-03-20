40c523cd430e3335204c1a6eac4a2c884b545b4f
Mettelund3 <Melu32113@edu.ucl.dk>
2023-03-20 14:07:31 +0100

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+//kommentar//
+const vandcykel = ref('');
+
+function onSubmit () {
+  alert(vandcykel.value);
+}
+</script>
+
+<template>
+  <!-- kommentar-->
+  <input type="text" v-model="vandcykel"/>
+  <button v-on:click="onSubmit">
+    Dette er en knap
+  </button>
+  {{  vandcykel }}
+</template>
